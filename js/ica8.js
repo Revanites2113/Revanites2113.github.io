6b7ccdf9b9604b17fdbc68e7564e08ecf1c4315c
Revanites2113 <joro2073@colorado.edu>
2022-10-11 11:21:12 -0600

    ica8

---
+++ js/ica8.js
@@ -0,0 +1,7 @@
+function gamerFunction() {
+    document.getElementById("gamer").style.color = "blue";
+}
+
+function showGamer() {
+    document.getElementById("gamerFace").style.display="block";
+}
